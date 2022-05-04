d817355dcf26c49c052b2965ea63232408b6386a
Zharkyn20 <61348606+Zharkyn20@users.noreply.github.com>
2022-05-04 19:57:00 +0600

    Insertion sort

---
+++ Chapter_2/insertion_sort.py
@@ -0,0 +1,12 @@
+def insertion_sort(x):
+    for j in range(1, len(x)):
+        key = x[j]
+        i = j - 1
+        while i >= 0 and x[i] > key:
+            x[i + 1] = x[i]
+            i -= 1
+        x[i + 1] = key
+    print(x)
+
+
+insertion_sort([-21, 90, -34, 7])
